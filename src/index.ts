8636b405decf29c51b05b914e66eda4f8248cc6b
READTalk <redtalk.app@hotmail.com>
2026-05-31 20:50:47 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
 	async fetch(request, env) {
 		const inputs = {
-			prompt: "realistic images asian casual girl in the badroom",
+			prompt: "realistic images, asian girls, casual styles, girly in the city",
 		};
 
 		const response = await env.AI.run(
