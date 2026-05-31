c3d69a12a775396b81917e8d099a5bfaf5a86ca8
READTalk <redtalk.app@hotmail.com>
2026-05-31 20:37:09 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
 	async fetch(request, env) {
 		const inputs = {
-			prompt: "cyberpunk Indonesian",
+			prompt: "asian casual girl velocity background",
 		};
 
 		const response = await env.AI.run(
